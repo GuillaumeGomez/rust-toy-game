b8c56b610a6f8ea11598f2920f3a70ee4673696d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-04-29 23:50:36 +0200

    Add first enemy

--- src/main.rs
+++ src/main.rs
@@ -388,7 +388,7 @@ struct Player<'a> {
 }
 
 impl<'a> Player<'a> {
-    fn new(texture_creator: &'a TextureCreator<WindowContext>) -> Player<'a> {
+    fn new(texture_creator: &'a TextureCreator<WindowContext>, x: i32, y: i32) -> Player<'a> {
         let tile_width = 23;
         let tile_height = 23;
         let mut actions_standing = Vec::with_capacity(4);
@@ -431,8 +431,8 @@ impl<'a> Player<'a> {
                     secondary: None,
                     movement: None,
                 },
-                x: 0,
-                y: 0,
+                x,
+                y,
                 total_health: 100,
                 health: 75,
                 total_mana: 100,
@@ -468,6 +468,90 @@ impl<'a> Player<'a> {
         if self.character.stamina < self.character.total_stamina {
             self.character.stamina += 1;
         }
+    }
+}
+
+struct Enemy<'a> {
+    character: Character<'a>,
+}
+
+impl<'a> Enemy<'a> {
+    fn new(texture_creator: &'a TextureCreator<WindowContext>, x: i32, y: i32) -> Enemy<'a> {
+        let mut actions_standing = Vec::with_capacity(4);
+
+        // front
+        actions_standing.push(
+            Dimension::new(Rect::new(0, 73, 28, 36), 0),
+        );
+        // left
+        actions_standing.push(
+            Dimension::new(Rect::new(0, 40, 44, 31), 0),
+        );
+        // right
+        actions_standing.push(
+            Dimension::new(Rect::new(0, 40, 44, 31), 0),
+        );
+        // back
+        actions_standing.push(
+            Dimension::new(Rect::new(0, 29, 29, 52), 0),
+        );
+        let mut actions_moving = Vec::with_capacity(4);
+        let tile_width = 1;
+        let tile_height = 1;
+        actions_moving.push(
+            (Dimension::new(Rect::new(15, 77, tile_width, tile_height), 32), 10),
+        );
+        actions_moving.push(
+            (Dimension::new(Rect::new(350, 77, tile_width, tile_height), 32), 10),
+        );
+        actions_moving.push(
+            (Dimension::new(Rect::new(350, 50, tile_width, tile_height), 32), 10),
+        );
+        actions_moving.push(
+            (Dimension::new(Rect::new(683, 77, tile_width, tile_height), 32), 10),
+        );
+
+        let mut surface =
+            Surface::from_file("resources/enemy1.png").expect("failed to load `resources/enemy1.png`");
+
+        let texture = texture_creator
+            .create_texture_from_surface(surface)
+            .expect("failed to build texture from surface");
+        // let texture = create_right_actions(&texture_creator, &actions_standing, &actions_moving);
+        let texture_handler = TextureHandler {
+            texture,
+            actions_standing,
+            actions_moving,
+        };
+
+        Enemy {
+            character: Character {
+                action: Action {
+                    direction: Direction::Front,
+                    secondary: None,
+                    movement: None,
+                },
+                x,
+                y,
+                total_health: 100,
+                health: 75,
+                total_mana: 100,
+                mana: 20,
+                total_stamina: 100,
+                stamina: 100,
+                xp_to_next_level: 1000,
+                xp: 150,
+                texture_handler,
+            }
+        }
+    }
+
+    fn draw(&mut self, canvas: &mut Canvas<Window>, screen: &Rect) {
+        self.character.draw(canvas, false, screen)
+    }
+
+    fn apply_move(&mut self, map: &Map) {
+        // todo
     }
 }
 
@@ -585,7 +669,8 @@ pub fn main() {
 
     let mut event_pump = sdl_context.event_pump().expect("failed to get event pump");
     let map = Map::new(&texture_creator, &mut rng);
-    let mut player = Player::new(&texture_creator);
+    let mut player = Player::new(&texture_creator, 0, 0);
+    let mut enemy = Enemy::new(&texture_creator, 50, 20);
     let hud = HUD::new(&texture_creator);
     let mut screen = Rect::new(
         player.character.x - WIDTH / 2,
@@ -641,6 +726,7 @@ pub fn main() {
         screen.x = player.character.x - WIDTH / 2;
         screen.y = player.character.y - HEIGHT / 2;
         map.draw(&mut canvas, &screen);
+        enemy.draw(&mut canvas, &screen);
         player.draw(&mut canvas, &screen);
         hud.draw(&player, &mut canvas);
 
